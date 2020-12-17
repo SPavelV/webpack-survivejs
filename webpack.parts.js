032effb42dae80c0fd50896c0568d4c0cc099e6a
Pavel Solovev <pashius2010@gmail.com>
2020-12-17 16:43:42 +0400

    loading styles

--- webpack.parts.js
+++ webpack.parts.js
@@ -16,3 +16,9 @@ exports.devServer = () => ({
 exports.page = ({ title }) => ({
   plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
 });
+
+exports.loadCss = () => ({
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+});
